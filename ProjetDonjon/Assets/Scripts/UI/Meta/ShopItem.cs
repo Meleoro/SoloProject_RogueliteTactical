a9e7127feadcac1dba6766debc66345b3dc9694c
Arthur <arthur.perigno@gmail.com>
2025-12-30 07:44:02 +0100

    [Update] Shop Feedbacks

--- ProjetDonjon/Assets/Scripts/UI/Meta/ShopItem.cs
+++ ProjetDonjon/Assets/Scripts/UI/Meta/ShopItem.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,24 +9,42 @@ public class ShopItem : MonoBehaviour
 {
     [Header("Parameters")]
     [SerializeField] private LootData itemData;
+    [SerializeField] private Color validColor;
+    [SerializeField] private Color invalidColor;
 
     [Header("Action")]
     public Action<ShopItem, LootData> OnValidClick;
 
+    [Header("Private Infos")]
+    private Vector3 originalPos;
+    private Color baseColor;
+
     [Header("References")]
     [SerializeField] private TextMeshProUGUI _priceText;
     [SerializeField] private Image _itemImage;
+    [SerializeField] private Image _itemColliderImage;
 
 
 
     private void Start()
     {
+        _itemImage.material = new Material(_itemImage.material);
+        _itemImage.material.SetVector("_TextureSize", new Vector2(_itemImage.sprite.texture.width, _itemImage.sprite.texture.height));
+
+        originalPos = _itemImage.rectTransform.localPosition;
+        baseColor = _priceText.color;
+
         LoadData(itemData);
     }
 
     private void LoadData(LootData data)
     {
         _itemImage.sprite = data.sprite;
+        _itemColliderImage.sprite = data.sprite;
+
+        _itemImage.SetNativeSize();
+        _itemColliderImage.SetNativeSize();
+
         _priceText.text = data.value.ToString();
     }
 
@@ -34,19 +53,71 @@ public class ShopItem : MonoBehaviour
 
     public void Hover()
     {
-        _itemImage.rectTransform.DOScale(Vector3.one * 1.15f, 0.15f).SetEase(Ease.OutBack);
+        _itemImage.DOComplete();
+
+        _itemImage.rectTransform.DOScale(Vector3.one * 1.2f, 0.15f).SetEase(Ease.OutCubic);
+        _itemImage.rectTransform.DOLocalMove(originalPos + Vector3.up * 10f, 0.15f).SetEase(Ease.OutCubic);
+        _itemImage.material.DOFloat(1f, "_OutlineSize", 0.15f).SetEase(Ease.OutCubic);
     }
 
     public void Unhover()
     {
-        _itemImage.rectTransform.DOScale(Vector3.one * 1f, 0.15f).SetEase(Ease.InBack);
+        _itemImage.DOComplete();
+        _itemImage.rectTransform.DOComplete();
+
+        StopAllCoroutines();
+
+        _itemImage.rectTransform.DOScale(Vector3.one * 1f, 0.15f).SetEase(Ease.OutCubic);
+        _itemImage.rectTransform.DOLocalMove(originalPos, 0.15f).SetEase(Ease.OutCubic);
+        _itemImage.material.DOFloat(0f, "_OutlineSize", 0.15f).SetEase(Ease.OutCubic);
     }
 
     public void Click()
     {
-        //if (InventoriesManager.Instance.CurrentCoins < itemData.value) return;
+        if (InventoriesManager.Instance.CurrentCoins < itemData.value)
+        {
+            _itemImage.DOComplete();
+            _itemImage.rectTransform.DOComplete();
+            StopAllCoroutines();
+
+            _itemImage.rectTransform.DOShakePosition(0.3f, 10.0f, 40, 90, false, true);
+            StartCoroutine(BouncePriceColorCoroutine(invalidColor));
+
+            return;
+        }
 
         OnValidClick?.Invoke(this, itemData);
+
+        _itemImage.DOComplete();
+        StopAllCoroutines();
+
+        StartCoroutine(BouncePriceColorCoroutine(validColor));
+        StartCoroutine(BounceBuyCoroutine());
+    }
+
+    #endregion
+
+
+    #region Game Feel
+
+    private IEnumerator BounceBuyCoroutine()
+    {
+        _itemImage.rectTransform.DOScale(Vector3.one * 0.8f, 0.1f);
+
+        yield return new WaitForSeconds(0.1f);
+
+        _itemImage.rectTransform.DOScale(Vector3.one * 1.2f, 0.15f).SetEase(Ease.OutCubic);
+    }
+
+    private IEnumerator BouncePriceColorCoroutine(Color color)
+    {
+        _priceText.DOColor(color, 0.1f);
+        _priceText.rectTransform.DOScale(Vector3.one * 0.8f, 0.1f);
+
+        yield return new WaitForSeconds(0.1f);
+
+        _priceText.DOColor(baseColor, 0.15f).SetEase(Ease.OutCubic);
+        _priceText.rectTransform.DOScale(Vector3.one * 1f, 0.15f).SetEase(Ease.OutCubic);
     }
 
     #endregion
